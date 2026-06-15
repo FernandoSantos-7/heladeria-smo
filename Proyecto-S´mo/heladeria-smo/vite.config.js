5dabfbee0bec0e19adbee0107c35e7ec72e00790
Fernando Santos <santosfernandoe225@gmail.com>
2026-06-15 15:16:39 -0300

    Configurar base path para resolver error 404

--- Proyecto-S´mo/heladeria-smo/vite.config.js
+++ Proyecto-S´mo/heladeria-smo/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Esto es lo que soluciona el error 404 de archivos no encontrados
 })
